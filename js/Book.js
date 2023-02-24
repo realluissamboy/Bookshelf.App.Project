0f05d50ede4c9bede7c411b8f2e8c15d04b79b34
Luis Samboy <luissamboy@icloud.com>
2023-02-24 11:00:45 -0800

    Completed comment field. Doing touchups and still need to complete styling.

--- js/Book.js
+++ js/Book.js
@@ -5,7 +5,8 @@ class Book {
     this.subject = subject;
     this.title = title;
     this.bookcover =
-      bookcover || "https://png.pngtree.com/png-clipart/20210311/original/pngtree-an-open-blue-cartoon-book-png-image_6019373.jpg";
+      bookcover ||
+      "https://png.pngtree.com/png-clipart/20210311/original/pngtree-an-open-blue-cartoon-book-png-image_6019373.jpg";
     this.comment = [];
   }
 
@@ -22,7 +23,21 @@ class Book {
     let author = document.createElement("p");
     author.textContent = `Written by ${this.author}`;
 
-    bookcard.append(bookcover, title, author);
+    let commentForm = document.createElement("form");
+    commentForm.setAttribute("action", "#");
+    commentForm.setAttribute("method", "POST");
+
+    let commentField = document.createElement("textarea");
+    commentField.setAttribute("name", "review");
+    commentField.setAttribute("placeholder", "Leave a review...");
+    commentForm.appendChild(commentField);
+
+    let submitButton = document.createElement("button");
+    submitButton.setAttribute("type", "submit");
+    submitButton.textContent = "Submit";
+    commentForm.appendChild(submitButton);
+
+    bookcard.append(bookcover, title, author, commentForm);
 
     return bookcard;
   }
